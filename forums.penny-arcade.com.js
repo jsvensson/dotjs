0c2a76d4c58418ecdf1394e71fe29aa87fc71f31
Johan Svensson <johan@atomicplayboy.net>
2011-07-15 17:38:33 +0200

    Initial commit

---
+++ forums.penny-arcade.com.js
@@ -0,0 +1,7 @@
+var link = $("<link>");
+link.attr({
+        type: 'text/css',
+        rel: 'stylesheet',
+        href: 'file:///home/echo/.js/forums.penny-arcade.com.css'
+});
+$("head").append(link);
