cc579eb6f8f1eb8475c1631ea8274bd71188c1dc
Johan Svensson <johan@atomicplayboy.net>
2011-07-22 20:17:21 +0200

    .js stuff
    * Inject stylesheet
    * Make brackets in thread titles bold

--- forums.penny-arcade.com.js
+++ forums.penny-arcade.com.js
@@ -1,3 +1,17 @@
+// Bind Ctrl-S to submit post
+
+$.getScript('jquery.hotkeys.js', function() {
+  /*
+  $(document).ready(function(){
+    $('#Form_Body').bind('keyup', 'ctrl-s', function(){
+      $('#Form_Comment').submit();
+    });
+  });
+  */
+});
+
+// Stylesheet injection
+
 var link = $("<link>");
 link.attr({
         type: 'text/css',
@@ -5,3 +19,13 @@ link.attr({
         href: 'http://dl.dropbox.com/u/569190/dotjs/forums.penny-arcade.com.css'
 });
 $("head").append(link);
+
+// Bold thread title brackets
+$(".DiscussionName a:first-child").html(function() {
+	var txt = $(this).html();
+	txt = txt.replace(/(\[.*\])/, "<b>$1</b>");
+	return(txt);
+});
+
+
+
