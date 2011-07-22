86acecc051e83db045edf3480f39cd11739847d5
Johan Svensson <johan@atomicplayboy.net>
2011-07-22 22:44:15 +0200

    New URL for compiled CSS

--- forums.penny-arcade.com.js
+++ forums.penny-arcade.com.js
@@ -3,7 +3,7 @@ var link = $("<link>");
 link.attr({
         type: 'text/css',
         rel: 'stylesheet',
-        href: 'http://dl.dropbox.com/u/569190/dotjs/forums.penny-arcade.com.css'
+        href: 'http://dl.dropbox.com/u/569190/git/dotjs/forums.penny-arcade.com.css'
 });
 $("head").append(link);
 
