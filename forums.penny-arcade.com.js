9088682a98c6ab34bdcb41cee3c0f16a17620944
Johan Svensson <johan@atomicplayboy.net>
2012-07-28 18:26:43 +0200

    Theme reboot
    Starting over from scratch with the new changes to Vanilla

--- forums.penny-arcade.com.js
+++ forums.penny-arcade.com.js
@@ -5,7 +5,7 @@ link.attr({
 	rel: 'stylesheet',
 	href: 'http://dl.dropbox.com/u/569190/git/dotjs/forums.penny-arcade.com.css'
 });
-// $("head").append(link);
+$("head").append(link);
 
 // Bold thread title brackets
 $(".DiscussionName a.Title").html(function() {
@@ -13,6 +13,4 @@ $(".DiscussionName a.Title").html(function() {
 	$txt = $txt.replace(/(\[.*?\])/g, "<b>$1</b>");
 	return($txt);
 });
-// Insert URL prompt button
-$(".ButtonBar").append('<div class="PromptUrl"><span>prompturl</span></div>');
 
